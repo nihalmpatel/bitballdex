a095f717be17234f36534bc2f5b6e083c0bb65c2
Nihal Patel <hello@nihalpatel.com>
2020-08-24 15:56:32 -0400

    Update mainnet config

--- app/config.py
+++ app/config.py
@@ -23,15 +23,15 @@ WS_PROVIDER_URL = environ.get("WS_PROVIDER_URL")
 ALLOWED_ORIGIN_SUFFIXES = environ.get("ALLOWED_ORIGIN_SUFFIXES",
                                       "localhost").split(",")
 
-#ED_CONTRACT_ADDR = '' # EthereumDEX Mainnet Contract
-ED_CONTRACT_ADDR = '0x81b045629630a10f4713036cec006b82e832ead7' # DEX Ropsten Contract
+ED_CONTRACT_ADDR = '0x546264ba4099b7e1f01632fe83a71a1ca2fa77b6' #  Mainnet Contract
+# ED_CONTRACT_ADDR = '0x81b045629630a10f4713036cec006b82e832ead7' # DEX Ropsten Contract
 
 with open('ethereumdex.abi.json') as f:
     import json
     ED_CONTRACT_ABI = json.load(f)
 
 ED_WS_SERVERS = [
-    "ws://62.77.152.225:8080/socket.io/?EIO=3&transport=websocket"
+    "ws://dex-bitball.com:8080/socket.io/?EIO=3&transport=websocket"
 ]
 
 POSTGRES_HOST = "postgres"
@@ -41,6 +41,6 @@ POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD")
 
 HTTP_ORDERS_ENDPOINT_SECRET = environ.get("HTTP_ORDERS_ENDPOINT_SECRET")
 
-FRONTEND_CONFIG_FILE = "http://62.77.152.225/config/main.json" # Ropsten Config
+FRONTEND_CONFIG_FILE = "http://dex-bitball.com/config/main.json" # Config
 
 STOPPED_TOKENS = ()
